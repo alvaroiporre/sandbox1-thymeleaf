ee09f06360b6c1ab6851e777cc81785bdd2fa802
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-04-13 19:22:48 -0400

    Add userDto

--- src/main/java/com/alvaro/example/example/controllers/UserRestController.java
+++ src/main/java/com/alvaro/example/example/controllers/UserRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alvaro.example.example.models.User;
+import com.alvaro.example.example.models.dto.UserDto;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,13 +18,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserRestController {
 
   @GetMapping("/details")
-  public Map<String, Object> details () {
+  public UserDto details () {
+    UserDto userDto = new UserDto();
     User user = new User("alvaro", "Java");
+    userDto.setUser(user);
+    userDto.setTitle("Using DTO");
 
-    Map<String, Object> body = new HashMap<>();
-    body.put("title", "title");
-    body.put("user", user);
-    return body;
+    
+    return userDto;
   }
+
+  // @GetMapping("/details")
+  // public Map<String, Object> details () {
+  //   User user = new User("alvaro", "Java");
+
+  //   Map<String, Object> body = new HashMap<>();
+  //   body.put("title", "title");
+  //   body.put("user", user);
+  //   return body;
+  // }
+
+  
 
 }
---
+++ src/main/java/com/alvaro/example/example/models/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.alvaro.example.example.models.dto;
+
+import com.alvaro.example.example.models.User;
+
+public class UserDto {
+  private String title;
+  private User user;
+  public String getTitle() {
+    return title;
+  }
+  public void setTitle(String title) {
+    this.title = title;
+  }
+  public User getUser() {
+    return user;
+  }
+  public void setUser(User user) {
+    this.user = user;
+  }
+}
