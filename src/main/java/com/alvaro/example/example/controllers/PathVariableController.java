322fae2ad455c2bb4062d3abd0246197e6e226ab
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-04-15 16:34:55 -0400

    Add create post to see the body

--- src/main/java/com/alvaro/example/example/controllers/PathVariableController.java
+++ src/main/java/com/alvaro/example/example/controllers/PathVariableController.java
@@ -3,10 +3,15 @@ package com.alvaro.example.example.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alvaro.example.example.models.User;
 import com.alvaro.example.example.models.dto.ParamsDto;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -18,6 +23,12 @@ public class PathVariableController {
     param.setMessage(message);
     param.setCode(code);
     return param;
+  }
+  
+  @PostMapping("/create")
+  public User create(@RequestBody User user) {
+      
+      return user;
   }
   
 }
